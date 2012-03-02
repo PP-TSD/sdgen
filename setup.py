aad9eb92863a2fcaf7158e3318d8356b58cb445d
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2012-03-02 17:34:58 +0100

    change field property

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 import os
 from setuptools import setup, find_packages
 
+PACKAGE_DIR = 'src'
+
 def read(file_name):
 	return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 setup(
 	name = "sdgen",
-	version = "0.0.1",
-	author = "Tomasz Zurkowski, Piotr Slatala, Marek Kuzora, Anna Fenster",
-	author_email = "doriath88@gmail.com piotr@sepio.pl marek.kuzora@gmail.com noireffe@gmail.com",
-	description = "",
-	license = "MIT",
-	zip_safe = False,
-	keywords = "diagram generator",
+	version = '0.0.1',
+	author = read("AUTHORS"),
+	keywords = "syntax diagram generator",
 	url = "https://github.com/PP-TSD/sdgen",
-	package_dir = {'':'src'},
-	packages = find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	package_dir = {'': PACKAGE_DIR},
 	long_description = read("README.md"),
-	classifiers = [],
+	packages = find_packages(PACKAGE_DIR, exclude=['ez_setup', 'examples', 'tests']),
+	zip_safe = False,
 )
