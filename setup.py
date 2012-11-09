fda404f1027e3bed2fb5f3ea6878757427f104fd
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2012-11-09 15:02:12 +0100

    fix version number

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup, find_packages
 PACKAGE_DIR = 'src'
 
 def read(file_name):
-	return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 setup(
-	name = "sdgen",
-	version = '0.0.3',
-	author = read("AUTHORS"),
-	keywords = "syntax diagram generator",
-	url = "https://github.com/PP-TSD/sdgen",
-	package_dir = {'': PACKAGE_DIR},
-	long_description = read("README.md"),
-	packages = find_packages(PACKAGE_DIR, exclude=['ez_setup', 'examples', 'tests']),
-	zip_safe = False,
+    name = "sdgen",
+    version = '0.0.2',
+    author = read("AUTHORS"),
+    keywords = "syntax diagram generator",
+    url = "https://github.com/PP-TSD/sdgen",
+    package_dir = {'': PACKAGE_DIR},
+    long_description = read("README.md"),
+    packages = find_packages(PACKAGE_DIR, exclude=['ez_setup', 'examples', 'tests']),
+    zip_safe = False,
 )
