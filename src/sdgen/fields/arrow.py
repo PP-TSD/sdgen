d29eb2633af7397b9ecef9336d6e9e74d56c5b79
Jakub Kędzierski <kubked@gmail.com>
2013-03-18 21:08:10 +0100

    Simple arrow (untested)

---
+++ src/sdgen/fields/arrow.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from _field import Field
+
+
+class Arrow(Field):
+    def __init__(self, size, thickness=1, marker="normal", fill="black"):
+        """Base arrow class.
+    
+        Args:
+            size (tuple): width, height of rectangle.
+    
+        Kwargs:
+            thickness (int): thickness of arrow (in pixels).
+            marker (str): size of arrowhead (normal|large|small)
+            fill (str): color (default black).
+        """
+        self.end = (max(size[0], 0), max(size[0], 0))
+        self.start = (max(-size[0], 0), max(-size[1], 0))
+        self.size = size
+        self.thickness = thickness
+        self.marker = marker
+        self.fill = fill
---
+++ src/sdgen/fields/simple_arrow.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import Image
+import ImageDraw
+
+from sdgen.utils.image_wrapper import ImageWrapper
+from arrow import Arrow
+
+
+class SimpleArrow(Arrow):
+    def to_png(self):
+        arrowhead_ratio = {
+                           "small": 1,
+                           "normal": 2,
+                           "large": 3
+                           }[self.marker]
+
+        arrow = 5 * arrowhead_ratio
+
+        image = Image.new('RGBA', self.size)
+        draw = ImageDraw.Draw(image)
+        
+        end_x, end_y = self.end
+        draw.line([self.start, self.end], width=self.thickness, color=self.fill)
+        draw.polygon([self.end, (end_x - arrow, end_y - arrow/2),
+                      (end_x - arrow, end_y + arrow/2)], fill=self.fill,
+                     outline=self.fill)
+        return ImageWrapper(image, *self.size)
