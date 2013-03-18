84f898a396f9a2e1c21d5b24cf0002d246009935
Jakub Kędzierski <kubked@gmail.com>
2013-03-18 16:56:25 +0100

    Changed fields building convention

--- src/sdgen/fields/character.py
+++ src/sdgen/fields/character.py
@@ -10,13 +10,27 @@ from sdgen.config import config
 
 
 class Character(Field):
-    """Character field.
-
-    Render text.
-    """
-    def _get_font(self, font_type, size, typeface):
-        """Get font with given parameters.
+    def __init__(self, text, font_type='Arial', size=10, typeface='normal'):
+        """Render text (without paddings).
+    
+        Args:
+            text (str): Text, which should be rendered.
+    
+        Kwargs:
+            font_type (str): Name of font type, ex. 'arial'.
+            size (int): Font size in points.
+            typeface (str): Font typeface, ex. 'bold italic'.
+    
+        Returns:
+            Image. Rendered image.
         """
+        self.text = text
+        self.font_type = font_type
+        self.size = size
+        self.typeface = typeface
+
+    def _get_font(self, font_type, size, typeface):
+        """Get font with given parameters."""
         font_type = font_type.lower()
         for directory in config.get('fonts', 'directories').split():
             for (dirpath, dirnames, filenames) in os.walk(directory):
@@ -30,23 +44,10 @@ class Character(Field):
                     return ImageFont.truetype(path, size)
         return ImageFont.load_default()
 
-    def to_png(self, text, font_type='Arial', size=10, typeface='normal'):
-        """Render png image with text (without paddings).
-
-        Args:
-            text (str): Text, which should be rendered.
-
-        Kwargs:
-            font_type (str): Name of font type, ex. 'arial'.
-            size (int): Font size in points.
-            typeface (str): Font typeface, ex. 'bold italic'.
-
-        Returns:
-            Image. Rendered image.
-        """
-        font = self._get_font(font_type, size, typeface)
-        image_size = font.getsize(text)
+    def to_png(self):
+        font = self._get_font(self.font_type, self.size, self.typeface)
+        image_size = font.getsize(self.text)
         image = Image.new('RGBA', image_size)
         draw = ImageDraw.Draw(image)
-        draw.text((0, 0), text, font=font)
+        draw.text((0, 0), self.text, font=font)
         return image
