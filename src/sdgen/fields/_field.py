755991fcbf80e60058f5e6357509beecf706f173
Jakub Kędzierski <kubked@gmail.com>
2013-03-24 17:13:12 +0100

    Must commit before pull

--- src/sdgen/fields/_field.py
+++ src/sdgen/fields/_field.py
@@ -1,5 +1,26 @@
 # -*- coding: utf-8 -*-
+import Image
+
 from sdgen.config import render_config, safeget
+from sdgen.utils.image_wrapper import ImageWrapper
+
+
+ANTIALIASING = 4
+
+
+def antialiasing(klass):
+    klass.pt_to_px = lambda *args, **kwargs: ANTIALIASING * klass.pt_to_px(*args, **kwargs) 
+    
+    def to_png(*args, **kwargs):
+        image_wrapper = klass.to_png(*args, **kwargs)
+        image = image_wrapper.get_image()
+        new_width = image_wrapper.get_width() / ANTIALIASING
+        new_height = image_wrapper.get_height() / ANTIALIASING
+        image.resize(new_width, new_height, Image.ANTIALIAS)
+        image_wrapper.scale_parameters(1.0/ANTIALIASING)
+        return image_wrapper
+    
+    klass.to_png = to_png
 
 
 class Field(object):
@@ -21,3 +42,6 @@ class Field(object):
 
     def to_svg(self):
         raise NotImplementedError()
+
+    def pt_to_px(self, points):
+        return points
--- src/sdgen/views/group.py
+++ src/sdgen/views/group.py
@@ -11,9 +11,6 @@ from sdgen.fields.flattener import Flattener
 
 
 class Group(View):
-    arrow_height = 1
-    arrow_width = 10
-    border_size = 1
 
     def get_arrow(self, x_offset, y_offset):
         arrow = SimpleArrow((self.arrow_width, self.arrow_height)).to_png()
@@ -21,64 +18,20 @@ class Group(View):
         return arrow
 
     def render(self):
-#        fields = []
-#        max_y = 0
-#
-#        for subfield in self.subfields:
-#            # image
-#            field = subfield.render()
-#            group_max_y = 0
-#
-#            # calculate max y
-#            for f in field:
-#                group_max_y = max(group_max_y, f.y + f.height)
-#
-#            # add field group to set
-#            fields.append((field, group_max_y))
-#            max_y = max(max_y, group_max_y)
-#
-#        layer = []
-#        x_offset = self.border_size
-#
-#        header = Character(self.name).to_png()
-#        header.set_position(self.border_size, self.border_size)
-#        header_height = header.height + self.border_size
-#
-#        for (field_group, group_max_y) in fields:
-#            layer.append(self.get_arrow(x_offset, max_y/2))
-#            x_offset += self.arrow_width
-#            group_max_x = 0
-#
-#            for field in field_group:
-#                group_max_x = max(field.x, group_max_x)
-#                field.apply_offset(x_offset, (max_y - group_max_y)/2 + header_height)
-#                layer.append(field)
-#
-#            x_offset += group_max_x
-#
-#        # end arrow
-#        layer.append(self.get_arrow(x_offset, max_y/2))
-#        x_offset += self.arrow_width
-#
-#        # border
-#        border = Rectangle((x_offset, max_y+header_height)).to_png()
-#        border.set_position(0, 0)
-#        layer.append(border)
-
         # render ImageWrappers of fields
         fields = [subfield.render() for subfield in self.subfields]
 
-        connections = []
-        # add connections
-        for i in range(len(fields) - 1):
-            if not isinstance(self.subfields[i], Connection) and not isinstance(self.subfields[i+1], Connection):
-                connection = Connection(fields[i], fields[i+1])
-                connections.append((connection, i+1))
+#        connections = []
+#        # add connections
+#        for i in range(len(fields) - 1):
+#            if not isinstance(self.subfields[i], Connection) and not isinstance(self.subfields[i+1], Connection):
+#                connection = Connection(fields[i], fields[i+1])
+#                connections.append((connection, i+1))
 
-        padding = 10
+#        padding = 10
         
-        for connection, position in reversed(connections):
-            fields.insert(position, connection.render())
+#        for connection, position in reversed(connections):
+#            fields.insert(position, connection.render())
 
         header = self.render_image(Character(self.name, color="white", background="black"))
         header_height = header.height + self.border_size
