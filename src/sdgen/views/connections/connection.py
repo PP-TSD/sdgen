939033d12d6fb873c500fa9858a982edd7409b2e
Mateusz Kurek <master.mateusz@gmail.com>
2013-03-24 18:19:13 +0100

    Added missing connections

---
+++ src/sdgen/views/connections/connection.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from .._view import View
+from sdgen.utils.image_wrapper import ImageWrapper
+from sdgen.fields.simple_arrow import SimpleArrow
+
+
+class Connection(View):
+    def __init__(self, first, second, *args, **kwargs):
+        """ First and second are ImageWrappers. """
+        self.first = first
+        self.second = second
+
+    def render(self):
+        return self.render_image(SimpleArrow((20, 0)))
