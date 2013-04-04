c2779d9d7670b9d692f4f5805052acc03e49822d
Mateusz Kurek <master.mateusz@gmail.com>
2013-04-04 21:20:32 +0200

    Quantity Above added

--- src/sdgen/views/quantity_above.py
+++ src/sdgen/views/quantity_above.py
@@ -1,6 +1,37 @@
 # -*- coding: utf-8 -*-
 
 from ._view import View
+from ..fields.character import Character
+from sdgen.fields.flattener import Flattener
+from sdgen.fields.rectangle import Rectangle
+
 
 class QuantityAbove(View):
-    pass
+    padding = 3
+
+    def add_children(self, children):
+        super(QuantityAbove, self).add_children(children)
+        assert len(self.subfields) == 1
+
+    def render(self):
+        quantity = self.render_image(Character(self.value))
+        subfield = self.render_subview(self.subfields[0])
+
+        width = max(quantity.get_width(), subfield.get_width())
+        height = sum((quantity.get_height(), self.padding, subfield.get_height()))
+
+        background = self.render_image(Rectangle((width, height), thickness=0))
+
+        quantity_x = (width - quantity.get_width())/2
+        subfield_x = (width - subfield.get_width())/2
+        subfield_y = quantity.get_height() + self.padding
+
+        quantity_above = Flattener(background, [(quantity, (quantity_x, 0)),
+                                                (subfield, (subfield_x, subfield_y))])
+        handlers = {
+            "left": subfield_y + subfield.get_handler('left'),
+            "right": subfield_y + subfield.get_handler('right')
+        }
+        rendered_fields = self.render_view(quantity_above)
+        rendered_fields[0].update_handlers(handlers)
+        return rendered_fields
