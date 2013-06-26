3ef1b299df449c82cf3d4aa742f73c81e73856ac
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2013-06-26 13:53:05 +0200

    Example that shows how spaces are presented

---
+++ samples/code/spaces.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.append(os.path.join('..', '..', 'src'))
+
+import sdgen
+
+data = {
+    "view": "Group",
+    "name": "Spaces example",
+    "children": [
+        {"view": "Terminal", "value": " "},
+        {
+            'children': [
+                {"view": "Terminal", "value": " B "},
+                {"view": "Terminal", "value": " C   "}
+            ],
+            "name": "Alternation BC    ",
+            "view": "Alternation"
+        },
+        {"view": "Terminal", "value": " D"}
+    ]
+}
+
+if __name__ == '__main__':
+    path = sys.argv[1] if len(sys.argv) == 2 else "output"
+    result = sdgen.to_png(data, path, overwrite=True)
