10f7aecdf42730bbdd8c039ffb8ebb75af55afb4
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2014-01-19 22:07:18 +0100

    set default ppi to 300

--- src/sdgen/png/png_builder.py
+++ src/sdgen/png/png_builder.py
@@ -31,7 +31,7 @@ class PNGBuilder(Builder):
         View.renderer = "to_png"
 
         default_png_config = {
-            'ppi': 600,
+            'ppi': 300,
             'scale': 1
         }
         png_config = safeget(render_config, "png", default_png_config)
