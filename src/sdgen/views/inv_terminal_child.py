c02c38c6f33471a2dc8b90717f45708bb403355a
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2014-01-19 22:09:30 +0100

    do not change spaces in Header

--- src/sdgen/views/inv_terminal_child.py
+++ src/sdgen/views/inv_terminal_child.py
@@ -27,4 +27,11 @@ class InvTerminalChild(Terminal):
         return self.render_view(text)
 
 class InvHeaderChild(InvTerminalChild):
-    pass
+    def render(self):
+        text = self.get_field(Character,
+                              self.value,
+                              font_color=self.font_color,
+                              background=self.background,
+                              marked_font_color=self.marked_font_color,
+                              marked_background=self.marked_background)
+        return self.render_view(text)
